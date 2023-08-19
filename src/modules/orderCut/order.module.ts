304f1a523699df9be481a4644ceeaa4a589683e4
vfa-tienld <tienld@gmail.com>
2023-08-20 01:45:15 +0700

    remove 1

--- src/modules/orderCut/order.module.ts
+++ src/modules/orderCut/order.module.ts
@@ -2,16 +2,16 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderCutEntity } from 'src/entities/orderCut.entity';
 import { AuthMiddleware } from 'src/shared/middlewares/auth.midleware';
-import { OrderCotroller } from './order.controller';
+import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([OrderCutEntity])],
-  controllers: [OrderCotroller],
+  controllers: [OrderController],
   providers: [OrderService],
 })
 export class OrderModule implements NestModule {
   public configure(consumer: MiddlewareConsumer) {
-    //consumer.apply(AuthMiddleware).forRoutes(OrderCotroller);
+    //consumer.apply(AuthMiddleware).forRoutes(OrderController);
   }
 }
