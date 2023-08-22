89cfe278d252ac225d4960050fc79451ee228396
loivd2001 <loivd@vitalify.asia>
2023-08-23 02:36:13 +0700

    apply middleware into categoty

--- src/modules/categories/category.module.ts
+++ src/modules/categories/category.module.ts
@@ -1,12 +1,24 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
 import { CategoriesEntity } from '../../entities/categories.entity';
+import { AuthMiddleware } from 'shared/middlewares/auth.midleware';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CategoriesEntity])],
+  imports: [
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '1d' },
+    }),
+    TypeOrmModule.forFeature([CategoriesEntity]),
+  ],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
-export class CategoryModule {}
+export class CategoryModule implements NestModule {
+  public configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(CategoryController);
+  }
+}
